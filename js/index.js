90572772ef1879790011a10f369f2e5d4713c0a0
Salim <salimchengly@gmail.com>
2021-03-10 01:04:19 +0100

    Work on product.js
    focus on the opening of my products with javascript.

--- js/index.js
+++ js/index.js
@@ -6,36 +6,50 @@ fetch(url)
         console.log(response);
         response.forEach(element => {
 
-            var prod = document.createElement("div");
+            const products = document.getElementById("products")
+
+            var prod = document.createElement("ul");
             prod.setAttribute("class", "produit")
-            products.append(prod);
-
-
+            
+            var imageUrl = document.createElement("img");
+            imageUrl.setAttribute('class', "cam_img");
+            imageUrl.src = element.imageUrl
+            
+            var textContent = document.createElement("div");
+            textContent.setAttribute("class", "cam_text");
+            
             var nom = document.createElement("h2");
             nom.innerHTML = element.name
             nom.setAttribute("class", "cam_title")
-            prod.append(nom)            
+            
+            var price = document.createElement("p");
+            price.innerHTML = element.price
+            price.setAttribute("class", "cam_price");
+            price.textContent = (parseInt(element.price, 10) / 100) + ",00 €";
+            
+            var description = document.createElement("p")
+            description.innerHTML = element.description
+            
+            var lien = document.createElement("button")
+            lien.innerHTML = "<a href='products.html?id="+element._id+"'>Voir le produit</a>";
+            lien.setAttribute("class", "link_cam");
+
+            let icon = document.createElement("i")
+            icon.innerHTML = '<i class="fas fa-search" aria-hidden="true"></i>';
+            
+            var listCam = document.createElement("li")
+            //listCam.innerHTML = "<li></li>";
             
             
-           var imageUrl = document.createElement("img");
-           imageUrl.setAttribute('class', "cam_img");
-           imageUrl.src = element.imageUrl
-           prod.append(imageUrl) 
-
-           var price = document.createElement("prix");
-           price.innerHTML = element.price
-           price.setAttribute("class", "cam_price");
-           prod.append(price);
-           price.textContent = (parseInt(element.price, 10) / 100) + ",00 €";
-
-           var description = document.createElement("des")
-           description.innerHTML = element.description
-           prod.append(description);
-
-           var lien = document.createElement("p")
-           lien.innerHTML = "<a href='products.html?id="+element._id+"'>Voir le produit</a>";
-           prod.append(lien)
-
+           products.append(prod);
+           prod.append(listCam);
+           listCam.append(imageUrl);
+           listCam.append(textContent);
+           textContent.append(nom);            
+           textContent.append(price);
+           //listCam.append(description);
+           textContent.append(lien);
+           lien.append(icon);
 
         });
     })
--- js/product.js
+++ js/product.js
@@ -1,7 +1,33 @@
-const queryString = window.location.search
+/*const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
-const id = urlParams.get("id")
+const camId = urlParams.get("id")*/
 
+const urlParams = new URLSearchParams(window.location.search);
+const camId = urlParams.get("id");
+
+let dataProduct;
+
+async function productSelected() {
+    fetch(url + "/" + camId).then(function(response){
+        response.json().then(function(data){
+            dataProduct = data;
+
+            let desContainer = create("div", "class", "des_container");
+            let desName = create("h2", "class", "des_name");
+            let desPrice = create("p", "class", "des_price");
+            let desImg = create("img", "src", data.imageUrl);
+            let desText = create("p", "class", "des_text");
+
+            desImg.setAttribute("alt", "Image de la caméra");
+            desImg.setAttribute("class", "des_img");
+
+            desName.textContent = data.name;
+            desPrice.textContent = data.price / 100 + " " + ",00 €";
+            desText.textContent = data.description;
+        })
+    })
+}
+/*
 var xhr = new XMLHttpRequest();
 
 xhr.onreadystatechange = function() {
@@ -17,6 +43,6 @@ xhr.onreadystatechange = function() {
 
 
 xhr.open("GET", "http://localhost:3000/api/cameras/"+ id, true);
-xhr.send();
+xhr.send();*/
 
-console.info(id)
+console.info(camId)
